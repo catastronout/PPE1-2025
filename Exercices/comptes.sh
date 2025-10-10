f7467eef71bbdfff0ce5237a30d7b367b0bfbe2b
catastronout <sarah.nart@outlook.fr>
2025-10-10 14:58:57 +0200

    modification du script 'comptes.sh' (exercice 1) pour utiliser une boucle

--- Exercices/comptes.sh
+++ Exercices/comptes.sh
@@ -1,10 +1,17 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-echo "Nombre de lignes contenant 'Location' pour l'année 2016 :" 
-cat ann/2016/*/*.ann | grep "Location" | wc -l
+# echo "Nombre de lignes contenant 'Location' pour l'année 2016 :" 
+# cat ann/2016/*/*.ann | grep "Location" | wc -l
 
-echo "Nombre de lignes contenant 'Location' pour l'année 2017 :"
-cat ann/2017/*/*.ann | grep "Location" | wc -l
+# echo "Nombre de lignes contenant 'Location' pour l'année 2017 :"
+# cat ann/2017/*/*.ann | grep "Location" | wc -l
 
-echo "Nombre de lignes contenant 'Location' pour l'année 2018 :"
-cat ann/2018/*/*.ann | grep "Location" | wc -l
+# echo "Nombre de lignes contenant 'Location' pour l'année 2018 :"
+# cat ann/2018/*/*.ann | grep "Location" | wc -l
+
+
+for ANNEE in 2016 2017 2018
+do
+    echo "Nombre de lignes contenant 'Location' pour l'année "$ANNEE""
+    cat ann/$ANNEE/*/*.ann | grep "Location" | wc -l
+done
